48e4ad00347fc304bbe937437858367768f27d7b
Citronart <alizee.nouvel2@gmail.com>
2024-02-13 17:01:27 +0100

    je teste des trucs ENCORE

---
+++ Renaut.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Renaut" tilewidth="32" tileheight="32" tilecount="960" columns="40">
+ <image source="tilesets/Tilemap.png" width="1296" height="768"/>
+</tileset>
